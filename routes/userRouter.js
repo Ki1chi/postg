af3aae57993b5450caa38f195a325265762e4d2f
Nori <nori.terada9@gmail.com>
2024-09-21 16:27:13 -0400

    -created database

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const userRouter = Router();
 
 userRouter.get('/', (req, res) => {
-    res.send('hello')
+    res.send('hello w')
 })
 
 module.exports = userRouter;
